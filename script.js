a9b7c448e1303e5d7d01469896b38d8a97d82897
ParagVadher <vadherparag@gmail.com>
2022-08-11 21:58:37 +0530

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 
+document.addEventListener('keyup', jump)
+
 function jump(){
     if (character.classList != "animate"){
         character.classList.add("animate");
